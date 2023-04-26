10b0e78e6127381eabc71904797f90ac2419929c
Truong <truong.huy@broeskamp.com>
2023-04-26 15:07:58 +0200

    Initial Commit

---
+++ Xcode Test Project/Xcode_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Test_ProjectApp.swift
+//  Xcode Test Project
+//
+//  Created by admin on 26.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
